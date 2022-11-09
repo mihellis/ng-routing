5f66374c01c74f2858a1b6382e379b4de0976a14
spetsiotis <michail.spetsiotis@ext.ema.europa.eu>
2022-11-09 18:46:19 +0200

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
+import { SellerInfoComponent } from './seller-info/seller-info.component';
 
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'product/:id', component: ProductComponent,
+  children: [
+    {path: '', component: ProductDescriptionComponent},
+    {path: 'seller/:id', component: SellerInfoComponent},
+  ]
+  
+}
+  ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    ProductDescriptionComponent,
+    SellerInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers:[{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
